bde9a46958e9d22982df5a651bd690740a948c89
Pinky <dir.michaellin@gmail.com>
2016-12-05 15:01:58 +1100

    Added assets and dassets and disguised tick box

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+console.log("DAMN DANIEL");
+  $( ".about" ).click(function() {
+    console.log("About has been clicked");;
+  });
+
+  $( ".work" ).click(function() {
+    console.log("Work has been clicked");;
+  });
+
+  $( ".contact" ).click(function() {
+    console.log("Contact has been clicked");;
+  });
+});
